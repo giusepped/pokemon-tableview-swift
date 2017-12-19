cd271c76b42774d3ba870ca77ece0a64e8a54e8a
Giuseppe de Santis <giusdesan@gmail.com>
2017-12-19 15:58:59 +0100

    complete solution

--- PokemonTableView/ViewController.swift
+++ PokemonTableView/ViewController.swift
@@ -4,18 +4,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let pokemons = Pokemon.pomemons
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.pokemons.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell")!
+        let pokemon = pokemons[indexPath.row]
+        
+        cell.textLabel?.text = pokemon.name
+        cell.imageView?.image = UIImage(named: pokemon.imageName)
+        
+        if let detailTextLabel = cell.detailTextLabel {
+            detailTextLabel.text = "Type : \(pokemon.type)"
+        }
+        
+        return cell
     }
-
-
+    
 }
 
